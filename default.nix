61af97bf85d069f0ab610e2edb088bcc9fad7807
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-02-18 11:18:49 +0100

    Remove MKL altogether

--- default.nix
+++ default.nix
@@ -2,14 +2,7 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {
     overlays = [
-      (self: super: {
-        blas = super.blas.override {
-          blasProvider = self.mkl;
-        };
-        lapack = super.lapack.override {
-          lapackProvider = self.mkl;
-        };
-      })
+      (self: super: { })
     ];
   };
 in
@@ -21,22 +14,4 @@ pkgs.mkShell {
     python3.pkgs.scipy
     python3.pkgs.jupyterlab
   ];
-
-  shellHook = ''
-    if [ ! -f $HOME/.dockerbuildphase ]; then
-      touch $HOME/.dockerbuildphase
-      export DOCKER_BUILD_PHASE=true
-    fi
-
-    if [ "$DOCKER_BUILD_PHASE" = true ]; then
-      echo "Do some action in build phase"
-      echo "{ allowUnfree = true; }" > ~/.config/nixpkgs/config.nix
-    fi
-
-    if [ "$DOCKER_BUILD_PHASE" = false ]; then
-      echo "Do some action in run phase like start db"
-    fi
-
-    echo "Do some action in both phases"
-  '';
 }
