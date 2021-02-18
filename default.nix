3eb266927e4c91ed219dff90a86543a290eefdb1
Roberto Di Remigio <roberto.diremigio@gmail.com>
2021-02-18 11:51:29 +0100

    Add postBuild

--- default.nix
+++ default.nix
@@ -14,19 +14,4 @@ pkgs.mkShell {
     python3.pkgs.scipy
     python3.pkgs.jupyterlab
   ];
-
-  shellHook = ''
-    if [ ! -f $HOME/.dockerbuildphase ]; then
-      touch $HOME/.dockerbuildphase
-      export DOCKER_BUILD_PHASE=true
-    fi
-    if [ "$DOCKER_BUILD_PHASE" = true ]; then
-      echo "Do some action in build phase"
-    fi
-    if [ "$DOCKER_BUILD_PHASE" = false ]; then
-      echo "Do some action in run phase like start db"
-      echo "{ allowUnfree = true; }" > ~/.config/nixpkgs/config.nix
-    fi
-    echo "Do some action in both phases"
-  '';
 }
